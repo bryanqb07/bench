d1da18f67131426b2f938cabf621c9f4ab6255f8
Bryan Lynch <bml312@nyu.edu>
2019-02-18 12:46:31 -0500

    frontend auth done

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -6,33 +6,44 @@ import Root from './components/root';
 
 // WINDOW TESTING DELETE 
 
-import { postUser, postSession, deleteSession } from './util/api_util';
+// import { postUser, postSession, deleteSession } from './util/api_util';
 
-import { login, logout } from './actions/session_actions';
+// import { login, logout } from './actions/session_actions';
 
-window.testUser = {
-    username: "goodsheep",
-    password: "finger69",
-    email: "goodsheep@mail.com"
-};
+// window.testUser = {
+//     username: "goodsheep",
+//     password: "finger69",
+//     email: "goodsheep@mail.com"
+// };
 
-window.postUser = postUser;
-window.postSession = postSession;
-window.deleteSession = deleteSession;
-window.login = login;
-window.logout = logout;
+// window.postUser = postUser;
+// window.postSession = postSession;
+// window.deleteSession = deleteSession;
+// window.login = login;
+// window.logout = logout;
 
 ////////////////////
 
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    const store = configureStore();
-
-    // WINDOW TESTING //
-    window.getState = store.getState;
-    window.dispatch = store.dispatch;
-    //////////////////////
+    let store;
+    if(window.currentUser){
+        const preLoadedState = {
+            entities: {
+                users: {[window.currentUser.id]: window.currentUser}
+            },
+            session: { id: window.currentUser.id }
+        };
+        store = configureStore(preLoadedState);
+        delete window.currentUser;
+    }else{
+        store = configureStore();
+    }
+    // // WINDOW TESTING //
+    // window.getState = store.getState;
+    // window.dispatch = store.dispatch;
+    // //////////////////////
 
     ReactDOM.render(<Root store={store} />, root )
 });
--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,14 +4,15 @@ import { Route } from 'react-router-dom';
 import GreetingContainer from './greetings/greeting_container';
 import LoginFormContainer from './forms/login_form_container';
 import SignupFormContainer from './forms/signup_form_container';
+import { AuthRoute } from '../util/route_util';
 
 export default () => (
     <div>
         <header>
             <h1>Bench BnB</h1>
-            <GreetingContainer />
-            <Route path="/login" component={LoginFormContainer} />
-            <Route path="/signup" component={SignupFormContainer} />
+            <Route exact path="/"  component={GreetingContainer} />
+            <AuthRoute path="/login" component={LoginFormContainer} />
+            <AuthRoute path="/signup" component={SignupFormContainer} />
         </header>
     </div>
 )
